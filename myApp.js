a83a162e97841373fc5f69e12fccc22542e6509a
AhamedBilal <bilalazmy0@gmail.com>
2021-04-18 13:44:24 +0530

    body parser

--- myApp.js
+++ myApp.js
@@ -1,9 +1,11 @@
 var express = require('express');
 require('dotenv').config()
 var app = express();
+var bodyParser = require('body-parser');
 
 
 app.use("/public", express.static(__dirname +"/public"));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use((req, res, next) => {
  console.log(`${req.method} ${req.path} - ${req.ip}`)
  next();
