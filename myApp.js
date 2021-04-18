5a5e529618d0373fc70eda1faf05000ab6b2b920
AhamedBilal <bilalazmy0@gmail.com>
2021-04-18 12:00:15 +0530

    sending json

--- myApp.js
+++ myApp.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var app = express();
 
+
+app.use("/public", express.static(__dirname +"/public"));
 console.log("Hello World");
 
 app.get("/", (req, res) => {
  let path = __dirname + "/views/index.html";
  res.sendFile(path);
 });
+
+app.get("/json", (req, res) => {
+ res.json({"message": "Hello json"});
+});
+
 
 
 
